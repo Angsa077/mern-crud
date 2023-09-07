627cddc7b53e0773a0092d2f0c683deb9afeed8d
Angga Saputra <angsa0077@gmail.com>
2023-09-07 19:37:36 +0700

    route controller dan middleware product

--- backend/index.js
+++ backend/index.js
@@ -2,12 +2,20 @@ import express from "express";
 import cors from "cors";
 import session from "express-session";
 import dotenv from "dotenv";
-// import db from "./config/Database.js";
+import db from "./config/Database.js";
+import SequelizeStore from "connect-session-sequelize";
 import UserRoute from "./routes/UserRoute.js";
 import ProductRoute from "./routes/ProductRoute.js";
+import AuthRoute from "./routes/AuthRoute.js";
 dotenv.config();
 
 const app = express();
+
+const sessionStore = SequelizeStore(session.Store);
+
+const store = new sessionStore({
+    db: db
+});
 
 // (async()=>{
 //     await db.sync();
@@ -17,6 +25,7 @@ app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
     saveUninitialized: true,
+    store: store,
     cookie: {
         secure: 'auto'
     }
@@ -29,6 +38,9 @@ app.use(cors({
 app.use(express.json());
 app.use(UserRoute);
 app.use(ProductRoute);
+app.use(AuthRoute);
+
+// store.sync();
 
 app.listen(process.env.APP_PORT, ()=> {
     console.log('Server up and running...');
--- backend/routes/ProductRoute.js
+++ backend/routes/ProductRoute.js
@@ -6,13 +6,14 @@ import {
     updateProduct,
     deleteProduct
 } from "../controllers/Products.js";
+import { verifyUser } from "../middleware/AuthUser.js";
 
 const router = express.Router();
 
-router.get('/products', getProducts);
-router.get('/products/:id', getProductById);
-router.post('/products', createProduct);
-router.patch('/products/:id', updateProduct);
-router.delete('/products/:id', deleteProduct);
+router.get('/products', verifyUser, getProducts);
+router.get('/products/:id', verifyUser, getProductById);
+router.post('/products', verifyUser, createProduct);
+router.patch('/products/:id', verifyUser, updateProduct);
+router.delete('/products/:id', verifyUser, deleteProduct);
 
 export default router;
